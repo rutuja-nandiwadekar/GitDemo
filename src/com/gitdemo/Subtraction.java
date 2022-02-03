df0416694f3fb2370227e5da87e867825fd9e031
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-03 09:20:55 +0530

    Added : subtraction of two variables

---
+++ src/com/gitdemo/Subtraction.java
@@ -0,0 +1,12 @@
+package com.gitdemo;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 6;
+		int sub = a-b;
+		System.out.println("sub= "+sub);
+
+	}
+}
